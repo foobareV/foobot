0ea90ae2db7cb1c189537ddcc34438af39f4fb35
Kai Blöcker <mail@kaibloecker.de>
2012-04-21 22:10:33 +0200

    Help caching browsers detect status.png has changed

--- foobot.sh
+++ foobot.sh
@@ -13,24 +13,24 @@ DATETIME=`date +"%d.%m.%Y %H:%M"`
 case $FOOBAR_STATUS in
 0)
   if [ $LASTSTATUS -ne 0 ] ; then
-    rm htdocs/status.png && ln -s ../open.png htdocs/status.png;
+    rm htdocs/status.png && ln -s open.png htdocs/status.png;
     echo "open" > htdocs/api;
-    sed -e "s/{space}/${SPACE}/" -e "s/{status}/offen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}/<p>Wie lange ist noch jemand da\?<br \/>Ruf an: ${PHONE}<\/p>/" template.html > htdocs/index.html;
+    sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/open/" -e "s/{status}/offen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}/<p>Wie lange ist noch jemand da\?<br \/>Ruf an: ${PHONE}<\/p>/" template.html > htdocs/index.html;
     echo "0" > laststatus;
   fi
   ;;
 1)
   if [ $LASTSTATUS -ne 1 ] ; then
-    rm htdocs/status.png && ln -s ../closed.png htdocs/status.png;
+    rm htdocs/status.png && ln -s closed.png htdocs/status.png;
     echo "closed" > htdocs/api;
-    sed -e "s/{space}/${SPACE}/" -e "s/{status}/geschlossen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
+    sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/closed/" -e "s/{status}/geschlossen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
     echo "1" > laststatus;
   fi
   ;;
 *)
-  rm htdocs/status.png && ln -s ../error.png htdocs/status.png;
+  rm htdocs/status.png && ln -s error.png htdocs/status.png;
   echo "error" > htdocs/api;
-  sed -e "s/{space}/${SPACE}/" -e "s/{status}/Fehler/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
+  sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/error/" -e "s/{status}/Fehler/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
   echo "5" > laststatus;
   ;;
 esac
