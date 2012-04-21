3241c89662a54594dd9421ec50b5dd0efa5e2c69
Kai Blöcker <mail@kaibloecker.de>
2012-04-21 22:17:25 +0200

    Restructured foobot.sh with tabs instead of blanks

--- foobot.sh
+++ foobot.sh
@@ -12,30 +12,30 @@ DATETIME=`date +"%d.%m.%Y %H:%M"`
 
 case $FOOBAR_STATUS in
 0)
-  if [ $LASTSTATUS -ne 0 ] ; then
-    rm htdocs/status.png 2> /dev/null;
-    ln -s open.png htdocs/status.png;
-    echo "open" > htdocs/api;
-    sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/open/" -e "s/{status}/offen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}/<p>Wie lange ist noch jemand da\?<br \/>Ruf an: ${PHONE}<\/p>/" template.html > htdocs/index.html;
-    echo "0" > laststatus;
-  fi
-  ;;
+	if [ $LASTSTATUS -ne 0 ] ; then
+		rm htdocs/status.png 2> /dev/null;
+		ln -s open.png htdocs/status.png;
+		echo "open" > htdocs/api;
+		sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/open/" -e "s/{status}/offen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}/<p>Wie lange ist noch jemand da\?<br \/>Ruf an: ${PHONE}<\/p>/" template.html > htdocs/index.html;
+		echo "0" > laststatus;
+	fi
+	;;
 1)
-  if [ $LASTSTATUS -ne 1 ] ; then
-    rm htdocs/status.png 2> /dev/null;
-    ln -s closed.png htdocs/status.png;
-    echo "closed" > htdocs/api;
-    sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/closed/" -e "s/{status}/geschlossen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
-    echo "1" > laststatus;
-  fi
-  ;;
+	if [ $LASTSTATUS -ne 1 ] ; then
+		rm htdocs/status.png 2> /dev/null;
+		ln -s closed.png htdocs/status.png;
+		echo "closed" > htdocs/api;
+		sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/closed/" -e "s/{status}/geschlossen/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
+		echo "1" > laststatus;
+	fi
+	;;
 *)
-  rm htdocs/status.png 2> /dev/null;
-  ln -s error.png htdocs/status.png;
-  echo "error" > htdocs/api;
-  sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/error/" -e "s/{status}/Fehler/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
-  echo "5" > laststatus;
-  ;;
+	rm htdocs/status.png 2> /dev/null;
+	ln -s error.png htdocs/status.png;
+	echo "error" > htdocs/api;
+	sed -e "s/{space}/${SPACE}/" -e "s/{status_img}/error/" -e "s/{status}/Fehler/" -e "s/{datetime}/${DATETIME}/" -e "s/{phone}//" template.html > htdocs/index.html;
+	echo "5" > laststatus;
+	;;
 esac
 
 exit 0
